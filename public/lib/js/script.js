8dcf7bb1d56c4e081515ba19a44f15a6877b1202
Kevin Knoll <knollkevin@gmail.com>
2014-08-10 21:50:23 -0400

    Add canvas and drawing logic

--- public/lib/js/script.js
+++ public/lib/js/script.js
@@ -2,6 +2,19 @@
 
 (function (w, d) {
   var socket = w.io.connect(d.location.href);
+  var canvas;
+  var ctx;
+  var state = {
+    drawing: false,
+    prev: {
+      x: 0,
+      y: 0
+    },
+    curr: {
+      x: 0,
+      y: 0
+    }
+  };
 
   socket.on('message', function (data) {
     console.log('message recieved:', data.message);
@@ -12,4 +25,55 @@
   }
 
   sendCommand('connected!');
+
+  function init () {
+    canvas = d.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+
+    bindEvents();
+  }
+
+  function bindEvents () {
+    canvas.addEventListener('mousemove', trace, false);
+    canvas.addEventListener('mousedown', start, false);
+    canvas.addEventListener('mouseup', stop, false);
+    canvas.addEventListener('mouseout', stop, false);
+  }
+
+  function start (e) {
+    state.drawing = true;
+    findxy(e);
+  }
+
+  function stop (e) {
+    state.drawing = false;
+  }
+
+  function trace (e) {
+    if (state.drawing) {
+      findxy(e);
+      draw();
+    }
+  }
+
+  function findxy (e) {
+    state.prev.x = state.curr.x;
+    state.prev.y = state.curr.y;
+    state.curr.x = e.clientX - canvas.offsetLeft;
+    state.curr.y = e.clientY - canvas.offsetTop;
+  }
+
+  function draw () {
+    if (!state.drawing) {
+      return;
+    }
+
+    ctx.beginPath();
+    ctx.moveTo(state.prev.x, state.prev.y);
+    ctx.lineTo(state.curr.x, state.curr.y);
+    ctx.stroke();
+    ctx.closePath();
+  }
+
+  init();
 })(window, document);
